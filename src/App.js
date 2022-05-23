3c04ec6b16df0fcab50a824915d826305b1cb989
shamaskaramat <shams4195140@gmail.com>
2022-05-23 13:05:19 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Resturent from "./Components/Basic/Resturent";
+
+function App() {
+  return (
+    <>
+    <Resturent/>
+    </>
+    
+  );
+}
+
+export default App;
---
+++ src/Components/Basic/Resturent.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import'./Style.css';
+import Menu from './menuApi'
+import MenuCard from "./MenuCard";
+
+const uniqueList = [
+  ...new Set(
+    Menu.map((curElem) => {
+      return curElem.category;
+    })
+  ),
+  "All",
+];
+
+const Resturent = () => {
+    const [menuData, setMenuData] = useState(Menu);
+    const filterItem = (category)=>{
+        const updatedList=Menu.filter((curElem)=>{
+            return curElem.category===category;
+        }
+        )
+        setMenuData(updatedList);
+    };
+
+    return (
+        
+           <>
+           <nav className="navbar">
+               <div className="btn-group">
+                   <button className="btn-group__item"onClick={()=>filterItem("breakfast")}>Breakfast</button>
+                   <button className="btn-group__item"onClick={()=>filterItem("lunch")}>Lunch</button>
+                   <button className="btn-group__item"onClick={()=>filterItem("evening")}>evening</button>
+                   <button className="btn-group__item"onClick={()=>filterItem("dinner")}>Dinner</button>
+                   <button className="btn-group__item"onClick={()=>setMenuData(Menu)}>All</button>
+               </div>
+            
+           </nav>
+              <MenuCard menuData={menuData}/>
+            </>
+       
+    )
+}
+
+export default Resturent
